f2d1a48218fb4a9711f4afb97d0ceb7e90362a70
HumphreyJ <joshhumphrey1@gmail.com>
2020-01-09 20:17:32 -0600

    Added some styling!

--- taco-time/src/components/Taco.jsx
+++ taco-time/src/components/Taco.jsx
@@ -8,8 +8,48 @@ import styled from 'styled-components';
     .newTaco {
         width: 85%;
         margin: 0 auto;
+        padding: 2%;
+        background-color: rgba(0,0,0, 0.8);
+        color: white;
+        text-shadow: 2px 2px 2px #111;
+
+        
+
+        
+
+        .tacoComponent {
+            width: 85%;
+            margin: 0 auto;
+
+            .recipe {
+                font-family: 'Roboto', sans-serif;
+                font-size: 1.2em;
+                letter-spacing: .1em;
+                font-weight: bold;
+            }
+
+        }
+
+        
     }
- 
+
+    .welcome {
+        font-size: 2.4em;
+        width: 80%;
+        margin: 0 auto;
+        padding: 25% 0%;
+        background-color: rgba(0,0,0, 0.5);
+        color: white;
+        text-shadow: 2px 2px 2px #111;
+    }
+
+    .getTaco {
+        background-color: white;
+        color: #111;
+        font-size: 1.4em;
+        font-family: 'Roboto', sans-serif;
+        margin-bottom: 1em;
+    }
  `
 const Taco = (props) => {
     return (
@@ -24,36 +64,38 @@ props.taco.base_layer ?
 <div className="newTaco">
     <div className="tacoComponent shell">
         <h3>{props.taco.shell.name}</h3>
-        <p>{props.taco.shell.recipe.replace(/\=/g, '').replace(props.taco.shell.name, '')}</p>
+        <p className='recipe'>{props.taco.shell.recipe.replace(/\=/g, '').replace(props.taco.shell.name, '')}</p>
 
     </div>
     
     <div className="tacoComponent base">
         <h3>{props.taco.base_layer.name}</h3>
-        <p>{props.taco.base_layer.recipe.replace(/\=/g, '').replace(props.taco.base_layer.name, '')}</p>
+        <p className='recipe'>{props.taco.base_layer.recipe.replace(/\=/g, '').replace(props.taco.base_layer.name, '')}</p>
     </div>
     
     <div className="tacoComponent seasoning">
         <h3>{props.taco.seasoning.name}</h3>
-        <p>{props.taco.seasoning.recipe.replace(/\=/g, '').replace(props.taco.seasoning.name, '')}</p>
+        <p className='recipe'>{props.taco.seasoning.recipe.replace(/\=/g, '').replace(props.taco.seasoning.name, '')}</p>
     </div>
     
     <div className="tacoComponent mixin">
         <h3>{props.taco.mixin.name}</h3>
-        <p>{props.taco.mixin.recipe.replace(/\=/g, '').replace(props.taco.mixin.name, '')}</p>
+        <p className='recipe'>{props.taco.mixin.recipe.replace(/\=/g, '').replace(props.taco.mixin.name, '')}</p>
         
     </div>
     
     <div className="tacoComponent condiment">
         <h3>{props.taco.condiment.name}</h3>
-        <p>{props.taco.condiment.recipe.replace(/\=/g, '').replace(props.taco.condiment.name, '')}</p>
+        <p className='recipe'>{props.taco.condiment.recipe.replace(/\=/g, '').replace(props.taco.condiment.name, '')}</p>
         
     </div>
     
    
 
 </div> 
-: <h3>Nothing Yet</h3>
+: <div className='welcome'>
+    <p>Welcome to Taco Time! Click the button above to generate a random taco complete with recipes!</p>
+</div>
     }
             
         </TacoWrap>
